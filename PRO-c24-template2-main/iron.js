bff6661b67e080af167d2104eb7156357f962249
vipint1 <81351539+vipint1@users.noreply.github.com>
2021-06-30 09:55:00 +0530

    Add files via upload

---
+++ PRO-c24-template2-main/iron.js
@@ -0,0 +1,27 @@
+class Iron {
+    constructor(x, y,width,height) {
+      var options = {
+        'density':30,
+        'friction': 3,
+        'restitution':0.8
+      };
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(3);
+      stroke('black')
+      fill('black')
+      rectMode(CENTER)
+      rect(0, 0, this.width, this.height);
+      pop();
+    };
+  };
